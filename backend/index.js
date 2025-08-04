d41ced461c9a0fc09fed1c34106b497870f1ad01
Mohit Patel <mohit.lifelayer.org@gmail.com>
2025-08-04 13:02:52 +0530

    feat: initialize backend and frontend structure with address management
    - Added .gitignore to exclude unnecessary files and directories.
    - Created backend entry point in index.js with Express server setup.
    - Implemented ManageAddresses component for address management in the frontend.
    - Updated package.json with necessary dependencies and scripts for development and production.

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv';
+import app from './app.js';
+import connectDB from './config/db.js';
+import path from 'path';
+import express from 'express';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+app.get(/^(?!\/api).*/, (req, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+});
+
+const startServer = async () => {
+  await connectDB();
+  app.listen(PORT, () => {
+    console.log(`Server running on port http://localhost:${PORT}`);
+  });
+};
+
+startServer();
