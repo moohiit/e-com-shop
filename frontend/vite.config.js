bda75a295a59aca1d005b6c1cd78d8e950ca53bf
Mohit Patel <mohit.lifelayer.org@gmail.com>
2025-08-04 13:17:07 +0530

    fix: Update Vite configuration for improved proxy handling

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
-const { VITE_BACKEND_URL, ENVIRONMENT } = import.meta.env;
+const { VITE_BACKEND_URL, ENVIRONMENT } = process.env;
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
